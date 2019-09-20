4c13c540dceebfb6312624c65cfddd1bf00f2c82
harry6396 <harrytyagi2371@gmail.com>
2019-09-20 23:09:36 +0530

    Added service

---
+++ Back-End/SchoolManagementAPI/helloworld/src/main/java/com/schoolmanagement/helloworld/PaymentRequest.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.schoolmanagement.helloworld;
+
+/**
+ *
+ * @author Prabhat
+ */
+public class PaymentRequest {
+    private int userId;
+    private String itemId;
+    private double discount;
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+}
